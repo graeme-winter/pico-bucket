39b6071265a65b6d978d99a9b0df1675aae50ee4
Graeme Winter <graeme.winter@gmail.com>
2022-05-14 09:13:02 +0100

    Blinking with duty cycle

---
+++ blink_duty_cycle/blink.c
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "blink.pio.h"
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, float duty);
+
+int main() {
+    setup_default_uart();
+
+    // todo get free sm
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &blink_program);
+    printf("Loaded program at %d\n", offset);
+
+    blink_pin_forever(pio, 0, offset, 25, 1000, 0.1);
+    blink_pin_forever(pio, 1, offset, 14, 1000, 0.1);
+}
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, float duty) {
+    blink_program_init(pio, sm, offset, pin);
+
+    printf("Blinking pin %d at %d Hz %f\n", pin, freq, duty);
+    int count = (clock_get_hz(clk_sys) / freq);
+    int nn = count * duty;
+    
+    pio->txf[sm] = (count - nn) - 3;
+    
+    // copy 32 bits from OSR to ISR
+    pio_sm_exec(pio, sm, pio_encode_pull(false, false));
+    pio_sm_exec(pio, sm, pio_encode_out(pio_isr, 32));
+    pio->txf[sm] = nn - 3;    
+    pio_sm_set_enabled(pio, sm, true);
+}
