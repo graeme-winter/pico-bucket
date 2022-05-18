30babba1a4e46a614c64ffa74236cf2f6b084c9e
Graeme Winter <graeme.winter@gmail.com>
2022-05-18 07:20:49 +0100

    Basic API

---
+++ i2c/api.py
@@ -0,0 +1,18 @@
+import random
+import smbus
+import struct
+import time
+
+ADDRESS = 0x42
+
+bus = smbus.SMBus(1)
+
+message = struct.pack("IIII", 100, 101, 102, 103)
+
+# write and read block data
+
+sent = list(message)
+
+bus.write_i2c_block_data(ADDRESS, 0x1, sent)
+
+bus.write_i2c_block_data(ADDRESS, 0x2, [])
