337b528d3f2bdfdecf2c5b69c50c9248db6a982a
Graeme Winter <graeme.winter@gmail.com>
2022-07-03 11:16:30 +0100

    Demangled Python

--- usb-device/dev_lowlevel_loopback.py
+++ usb-device/dev_lowlevel_loopback.py
@@ -1,34 +1,48 @@
-#!/ usr / bin / env python3
+#!/usr/bin/env python3
 
 #
-#Copyright(c) 2020 Raspberry Pi(Trading) Ltd.
+# Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
 #
-#SPDX - License - Identifier : BSD - 3 - Clause
+# SPDX-License-Identifier: BSD-3-Clause
 #
 
-#sudo pip3 install pyusb
+# sudo pip3 install pyusb
 
-import usb.core import usb.util
+import usb.core
+import usb.util
 
-#find our device
-    dev = usb.core.find(idVendor = 0x0000, idProduct = 0x0001)
+# find our device
+dev = usb.core.find(idVendor=0x0000, idProduct=0x0001)
 
-#was it found ?
-                            if dev is None : raise ValueError('Device not found')
+# was it found?
+if dev is None:
+    raise ValueError('Device not found')
 
-#get an endpoint instance
-                                                                  cfg = dev.get_active_configuration() intf = cfg[(0, 0)]
+# get an endpoint instance
+cfg = dev.get_active_configuration()
+intf = cfg[(0, 0)]
 
-                                                                                                                   outep = usb.util.find_descriptor(intf,
-#match the first OUT endpoint
-                                                                                                                                                    custom_match = lambda e : usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_OUT)
+outep = usb.util.find_descriptor(
+    intf,
+    # match the first OUT endpoint
+    custom_match= \
+        lambda e: \
+            usb.util.endpoint_direction(e.bEndpointAddress) == \
+            usb.util.ENDPOINT_OUT)
 
-                                                                                                                                                        inep = usb.util.find_descriptor(intf,
-#match the first IN endpoint
-                                                                                                                                                                                        custom_match = lambda e : usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_IN)
+inep = usb.util.find_descriptor(
+    intf,
+    # match the first IN endpoint
+    custom_match= \
+        lambda e: \
+            usb.util.endpoint_direction(e.bEndpointAddress) == \
+            usb.util.ENDPOINT_IN)
 
-                                                                                                                                                                                            assert inep is not None assert outep is not None
+assert inep is not None
+assert outep is not None
 
-                                                                                                                                                                                        test_string = "Hello World!" outep.write(test_string) from_device = inep.read(len(test_string))
+test_string = "Hello World!"
+outep.write(test_string)
+from_device = inep.read(len(test_string))
 
-                                                                                                                                                                                                                                     print("Device Says: {}".format(''.join([chr(x) for x in from_device])))
+print("Device Says: {}".format(''.join([chr(x) for x in from_device])))
