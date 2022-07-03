3a47f0ba8b19172e3bfb2a76e9ac27224d817a28
Graeme Winter <graeme.winter@gmail.com>
2022-07-03 14:05:35 +0100

    Chop around, c/f

--- usb-device/usbd.c
+++ usb-device/usbd.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
 #include "hardware/clocks.h"
+#include "hardware/pio.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
+int main() {
+  stdio_init_all();
 
+  puts("Hello, world!");
 
-int main()
-{
-    stdio_init_all();
-
-
-
-    puts("Hello, world!");
-
-    return 0;
+  return 0;
 }
