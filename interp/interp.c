682d640ffa833d78be2bf9f0cbf88a79298c9337
Graeme Winter <graeme.winter@gmail.com>
2022-10-04 06:37:17 +0100

    Trivial counter

--- interp/interp.c
+++ interp/interp.c
@@ -1,25 +1,23 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/interp.h"
 #include "hardware/dma.h"
+#include "pico/stdlib.h"
+#include <stdio.h>
 
+int main() {
+  stdio_init_all();
 
+  interp_config cfg = interp_default_config();
+  interp_set_config(interp0, 0, &cfg);
 
+  interp0->accum[0] = 0;
+  interp0->base[0] = 1;
 
-int main()
-{
-    stdio_init_all();
+  while (true) {
+    for (int j = 0; j < 1000000; j++) {
+      interp0->pop[0];
+    }
+    printf("%d\n", interp0->pop[0]);
+  }
 
-    // Interpolator example code
-    interp_config cfg = interp_default_config();
-    // Now use the various interpolator library functions for your use case
-    // e.g. interp_config_clamp(&cfg, true);
-    //      interp_config_shift(&cfg, 2);
-    // Then set the config 
-    interp_set_config(interp0, 0, &cfg);
-
-
-    puts("Hello, world!");
-
-    return 0;
+  return 0;
 }
